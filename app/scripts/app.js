e135d8496219ad53ecf79b569cb90db7e7f97440
Ravali Ginjupalli <ravali.ginjupalli@gmail.com>
2017-03-23 16:56:31 -0500

    feat: Add contactus page and add corresponding controllers.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('confusionApp', []).controller('MenuController', ['$scope', function('$scope') {
+angular.module('confusionApp', []) .controller('MenuController', ['$scope', function($scope) {
   $scope.tab = 1;
   $scope.filtText = '';
   
@@ -67,4 +67,29 @@ angular.module('confusionApp', []).controller('MenuController', ['$scope', funct
   $scope.isSelected = function (checkTab) {
     return ($scope.tab === checkTab);
   };
+}])
+
+.controller('ContactController', ['$scope', function($scope) {
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                        var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+                        $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+                                }])
+
+.controller('FeedbackController', ['$scope', function($scope) {
+            $scope.sendFeedback = function() {
+            console.log($scope.feedback);
+            if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+                $scope.invalidChannelSelection = true;
+                console.log('incorrect');
+            }
+            else {
+                  $scope.invalidChannelSelection = false;
+                  $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                       agree:false, email:"" };
+                  $scope.feedback.mychannel="";
+                  $scope.feedbackForm.$setPristine();
+                  console.log($scope.feedback);
+                }
+            };
 }]);
